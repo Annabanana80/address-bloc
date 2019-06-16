4b8cb1f656adfb76a05a02b08f2734c728017801
Anna Sanderson <anna.sanderson@csuglobal.edu>
2019-06-16 14:26:31 -0400

    added getDate method

---
+++ controllers/MenuController.js
@@ -0,0 +1,62 @@
+ const inquirer = require('inquirer');
+
+ module.exports = class MenuController {
+   constructor(){
+   	 this.mainMenuQuestions = [
+      {
+       type: "list",
+        name: "mainMenuChoice",
+        message: "Please choose from an option below: ",
+        choices: [
+          "Add new contact",
+          "Get Date",
+          "Exit"
+        ]
+      }
+    ];
+    this.contacts = [];
+   }
+
+  getDate(){
+    console.log(new Date());
+    this.main();
+  };
+
+   main(){
+     console.log(`Welcome to AddressBloc!`);
+     inquirer.prompt(this.mainMenuQuestions).then((response) => {
+       switch(response.mainMenuChoice){
+         case "Add new contact":
+           this.addContact();
+           break;
+         case "Exit":
+           this.exit();
+         case "Get Date":
+           this.getDate();
+           break;
+         default:
+           console.log("Invalid input");
+           this.main();
+       }
+     })
+     .catch((err) => {
+       console.log(err);
+     });
+   }
+
+
+   clear(){
+     console.log("\x1Bc");
+   }
+
+    addContact(){
+     this.clear();
+     console.log('addContact called');
+     this.main();
+   }
+
+   exit(){
+     console.log("Thanks for using AddressBloc!");
+     process.exit();
+   }
+ }
